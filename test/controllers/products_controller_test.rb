23bdde6c2ac2a4b4290b2e96a06bf0d27f8d7b21
ARGUMEDO1609 <noslenque931@gmail.com>
2023-11-20 20:50:31 -0500

    controller products

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,8 +1,38 @@
-require "test_helper"
+require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get products_index_url
+  test 'render a list of products' do
+    get products_path
+
     assert_response :success
+    assert_select '.product', 2
+  end 
+
+  test 'render a detailed product page' do 
+    get product_path(products(:tenis))
+
+    assert_response :success
+    assert_select '.title', 'jordan'
+    assert_select '.description', 'nuevos'
+    assert_select 'price', '250$'
   end
-end
+
+      test 'render a new product form' do
+     get new_product_path
+
+     assert_response :success
+     assert_select 'form'
+  end
+
+  test 'allow to create a new product' do 
+    post products_path, params: {
+      product: {
+        title: 'sandalia nike',
+        description: 'usada',
+        price: 84
+      }
+    }
+
+    assert_redirected_to products_path
+  end
+end    
